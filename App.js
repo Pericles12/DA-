112eaa5b9e847667beddc8ca8dc001661784cfb6
Pericles Junior de Carvalho <88382900+Pericles12@users.noreply.github.com>
2023-04-18 10:02:53 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './src/Routes'; 
+
+
+
+
+export default function App() {
+  return (
+    <NavigationContainer> 
+      <StatusBar backgroundColor="#38A69D" barStyle="dark-content"/>  
+    <Routes/> 
+    </NavigationContainer>
+  ); 
+}
+
+
